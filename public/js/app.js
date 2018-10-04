3df4fb47078ef64354e99f8eb86930540981e245
Schuyler Ankele <schuyler.ankele@charter.com>
2018-10-03 21:21:20 -0600

    Some updates. Saved template

--- public/js/app.js
+++ public/js/app.js
@@ -9,12 +9,19 @@ const fillItUp = data =>{
         data.forEach(element => {
             console.log(element);
             let story = `
-            <div class="row" id="${element._id}">
-                <div class="col s8">
+            <div class="row story" id="${element._id}">
+                <div class="col s6">
                     <a href="${element.link}" target="_blank"><h4>${element.title}</h4></a>
                 </div>
-                <div class="col s4"><img src="${element.image}"/></div>
-            </div>`;
+                <div class="col s3"><img class="storyImg" src="${element.image}"/></div>
+                <div class="col s3">
+                    <h5 class="saveBtn">Save for Later 
+                    <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </h5>
+                </div>
+                
+            </div>
+            `;
             $('#articles').append(story);    
     });
 };
