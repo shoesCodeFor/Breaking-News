cbd6bd7cab5433c4a9db413cc85e94c5c18ad989
Schuyler Ankele <schuyler.ankele@charter.com>
2018-10-16 23:49:09 -0600

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 var sources = require('../controller/index');
 
-
+// TODO Refactor to a readable format
 /* GET home page. */
 router.get('/scrape', function(req, res, next) {
   sources.cnet(res);
 });
 
+// TODO make this modular
 router.get('/articles', function(req, res, next) {
   sources.getNews(res);
 });
 
+// Get an article
 module.exports = router;
